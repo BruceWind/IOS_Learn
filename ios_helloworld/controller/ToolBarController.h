87040ee74835b4a4243973b5f32a7acb017b6407
weizongwei5 <weizongwei5@git.quanmin.tv>
2017-04-04 15:09:23 +0800

    change inject author name

---
+++ ios_helloworld/controller/ToolBarController.h
@@ -0,0 +1,13 @@
+//
+//  ToolBarController.h
+//  ios_helloworld
+//
+//  Created by wei on 2017/4/4.
+//  Copyright © 2017年 wei. All rights reserved.
+//
+
+#ifndef ToolBarController_h
+#define ToolBarController_h
+
+
+#endif /* ToolBarController_h */
---
+++ ios_helloworld/controller/ToolBarController.m
@@ -0,0 +1,9 @@
+//
+//  ToolBarController.m
+//  ios_helloworld
+//
+//  Created by wei on 2017/4/4.
+//  Copyright © 2017年 wei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
